387eedde2c17c9085b9fa34ba62387ebdddef9d0
Konstantin Shenderov <konstantins@jfrog.com>
2018-05-13 13:29:39 +0300

    Init commit

---
+++ go.mod
@@ -0,0 +1,4 @@
+module github.com/shenderov/dep-two
+
+require shenderov.github.io/dep-one v1.0.0
+
---
+++ hello.go
@@ -0,0 +1,10 @@
+package main // import "github.com/shenderov/dep-two"
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Println("This is dep two")
+
+}
