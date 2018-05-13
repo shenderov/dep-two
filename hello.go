adfba2124651ad572d9b7916c0a4ad4733f20402
shenderov <shenderov.k@gmail.com>
2018-05-13 17:26:23 +0300

    Update hello.go

--- hello.go
+++ hello.go
@@ -2,8 +2,10 @@ package deptwo // import "github.com/shenderov/dep-two"
 
 import (
 	"fmt"
+	"github.com/shenderov/dep-one"
 )
 
 func Hello() {
 	fmt.Println("This is dep two")
+	depone.Hello()
 }
